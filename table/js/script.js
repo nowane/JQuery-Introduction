cc9d90cb0189fe4d49bcb21d59b0babf902afc60
Yorick van den Hurk <nowane@gmail.com>
2021-05-30 17:31:38 +0000

    jQuery cards

--- table/js/script.js
+++ table/js/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
-     $("tr:odd").addClass("odd");
-     $("tr:even").addClass("even");
- });
+    $("#stream1_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream1").addClass('highlight_stream');
+   });
+   $("#stream2_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+   });
+   $("#stream3_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+   });
+}); 
